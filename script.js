630216d6b417781efa8341f57cee79667e084838
dhanie-ellah <dannygodwin2003@gmail.com>
2022-10-25 12:22:29 +0100

    form handling

--- script.js
+++ script.js
@@ -1,44 +1,24 @@
 const submitBtns = document.querySelectorAll(".submit");
 const inputs = document.querySelectorAll(".input");
-const modal = document.querySelectorAll(".modal");
+// const modal = document.querySelectorAll(".modal");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const forms = document.querySelectorAll(".form");
 
 forms.forEach((form) => {
   form.addEventListener("submit", (e) => {
-    // e.preventDefault();
     inputs.forEach((input) => {
       if (input.value == "") {
         input.style.borderColor = "red";
-      } else {
-        modal.forEach((modal) => {
-          modal.style.display = "flex";
-        });
       }
+      input.addEventListener('keyup', () => {
+        input.style.borderColor = "white"
+      })
+
       modalBtn.forEach((btn) => {
         btn.addEventListener("click", () => {
-          modal.forEach((modal) => {
-            modal.style.display = "none";
+          window.location = 'index.html'
           });
         });
       });
     });
-
-    // for (let i = 0; i < inputs.length; i++) {
-    //      if (inputs[i].value = "") {
-    //           inputs[i].style.border = "1px solid red";
-    //      } else {
-    //           modal.forEach((modal) => {
-    //                modal.style.display = "flex";
-    //           });
-    //           modalBtn.forEach((btn) => {
-    //                btn.addEventListener("click", () => {
-    //                modal.forEach((modal) => {
-    //                modal.style.display = "none";
-    //                });
-    //                });
-    //           });
-    //      }
-    // }
   });
-});
